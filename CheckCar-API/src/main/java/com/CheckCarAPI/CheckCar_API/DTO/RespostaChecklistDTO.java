2c6f0e8021d6f5e1e5c73c1c7dcff33de9c2b7c2
Lucas Silva <lucassilvarodrigss16@gmail.com>
2025-11-24 00:25:31 -0300

    "feat: DTO para transferência de dados entre front e back-end"

---
+++ CheckCar-API/src/main/java/com/CheckCarAPI/CheckCar_API/DTO/RespostaChecklistDTO.java
@@ -0,0 +1,68 @@
+package com.CheckCarAPI.CheckCar_API.DTO;
+
+public class RespostaChecklistDTO {
+    private Long idLote;
+    private Integer idUsuario;
+    private Integer idVeiculo;
+    private String tipo;
+    private Integer idPergunta;
+    private String observacao;
+    private String status;
+
+    public Long getIdLote() {
+        return idLote;
+    }
+
+    public void setIdLote(Long idLote) {
+        this.idLote = idLote;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Integer getIdVeiculo() {
+        return idVeiculo;
+    }
+
+    public void setIdVeiculo(Integer idVeiculo) {
+        this.idVeiculo = idVeiculo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getIdPergunta() {
+        return idPergunta;
+    }
+
+    public void setIdPergunta(Integer idPergunta) {
+        this.idPergunta = idPergunta;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
+
