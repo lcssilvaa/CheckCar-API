10ea243b759c51fc749a1b41e65a63edf0267f4f
Lucas Silva <lucassilvarodrigss16@gmail.com>
2025-11-24 00:21:02 -0300

    "feat: dto para transferência de dados entre front e back-end"

---
+++ CheckCar-API/src/main/java/com/CheckCarAPI/CheckCar_API/DTO/ChecklistDTO.java
@@ -0,0 +1,25 @@
+package com.CheckCarAPI.CheckCar_API.DTO;
+
+public class ChecklistDTO {
+    private Integer idUsuario;
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Integer getIdVeiculo() {
+        return idVeiculo;
+    }
+
+    public void setIdVeiculo(Integer idVeiculo) {
+        this.idVeiculo = idVeiculo;
+    }
+
+    private Integer idVeiculo;
+
+}
+
